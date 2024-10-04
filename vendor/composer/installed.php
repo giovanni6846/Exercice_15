33c1ef999f44b06dc1cb51c2fd478ef14c7131aa
Giovanni <giovanni6846@gmail.com>
2024-10-04 11:39:13 +0200

    Exercice 15 avec IMC et Orage

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/cafe',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5fe6e2640651eab8d0408893336f1d781d1e5c18',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/cafe' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5fe6e2640651eab8d0408893336f1d781d1e5c18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
